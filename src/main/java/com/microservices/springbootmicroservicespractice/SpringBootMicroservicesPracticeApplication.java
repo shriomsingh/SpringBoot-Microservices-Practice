7c34f0bc1404a2b231ce2f35e16e77e2b4d3b150
shriomsingh <13706240+shriomsingh@users.noreply.github.com>
2024-02-20 00:53:27 +0530

    Initial Commit

---
+++ src/main/java/com/microservices/springbootmicroservicespractice/SpringBootMicroservicesPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.springbootmicroservicespractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices/springbootmicroservicespractice/SpringBootMicroservicesPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.springbootmicroservicespractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
